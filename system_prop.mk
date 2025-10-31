eaeb3a6422e7d29ed4dcfdadf91bec993e5580dd
apple <105837597+380583067@users.noreply.github.com>
2025-10-31 14:05:48 +0000

    Fix formatting and spacing in system_prop.mk
    Updated formatting and spacing in system_prop.mk.

--- system_prop.mk
+++ system_prop.mk
@@ -1,4 +1,4 @@
-#ADB
+# ADB
 PRODUCT_PROPERTY_OVERRIDES += \
   persist.service.adb.enable=1 \
   persist.service.debuggable=1 \
@@ -6,7 +6,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
   persist.sys.root_access=2 \
   ro.secure=0 \
   ro.adb.secure=0 \
-  ro.debuggable = 1 \
+  ro.debuggable= 1
 
 # AptX
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -17,10 +17,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # BT
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bt.bdaddr_path=/system/vendor/etc/mocha_btmacaddr.txt
+    ro.bt.bdaddr_path=/vendor/etc/mocha_btmacaddr.txt
 
-
-#DIDIM
+# DIDIM
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.legacyencoder=true \
     media.stagefright.less-secure=true
@@ -35,7 +34,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
 
-  # fastbootd
+# Fastbootd
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.fastbootd.available=true
 
@@ -95,5 +94,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
     wifi.interface=wlan0 \
     ap.interface=wlan0 \
-    persist.tegra.nvmmlite = 1 \
-    persist.wlan.ti.calibrated = 0
+    persist.tegra.nvmmlite=1 \
+    persist.wlan.ti.calibrated=0
