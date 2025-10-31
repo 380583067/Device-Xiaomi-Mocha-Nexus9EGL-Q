b6921666d2ec59eb4fcb9c8da7723c2ae97da0a7
apple <105837597+380583067@users.noreply.github.com>
2025-10-31 14:42:53 +0000

    Remove GO inheritance from mocha.mk
    Removed GO product inheritance from mocha.mk.

--- mocha.mk
+++ mocha.mk
@@ -146,9 +146,6 @@ PRODUCT_PACKAGES += \
     libshim_zw \
     libshim_atomic
 
-#GO
-$(call inherit-product, device/xiaomi/mocha/go_mocha.mk)
-
 # Health HAL
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
